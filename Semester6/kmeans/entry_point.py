867b178dcff169c8b19f89eb24f8640725698a6e
Drunk Russian Gun <drunkrussiangun@gmail.com>
2020-09-30 12:47:02 +0300

    Декомпозировал метод k средних

--- Semester6/kmeans/entry_point.py
+++ Semester6/kmeans/entry_point.py
@@ -1,5 +1,5 @@
 import random
-from typing import List
+from typing import Dict, List
 
 from point2d import Point2D
 
@@ -24,4 +24,38 @@ def get_random_points(count: int) -> List[Point2D]:
 	return points
 
 
+# noinspection PyShadowingNames
+def get_initial_clusters(points: List[Point2D], clusters_count: int) -> List[Point2D]:
+	raise NotImplementedError()
+
+
+# noinspection PyShadowingNames
+def group_points_by_clusters(points: List[Point2D], clusters: List[Point2D]) -> Dict[Point2D, List[Point2D]]:
+	raise NotImplementedError()
+
+
+# noinspection PyShadowingNames
+def get_next_clusters(points: List[Point2D], current_clusters: List[Point2D]) -> List[Point2D]:
+	raise NotImplementedError()
+
+
+def clusters_groups_are_same(
+		points: List[Point2D],
+		first_clusters: List[Point2D],
+		second_clusters: List[Point2D]) -> bool:
+	raise NotImplementedError()
+
+
+# noinspection PyShadowingNames
+def clusterize(points: List[Point2D], clusters_count: int) -> Dict[Point2D, List[Point2D]]:
+	previous_clusters = get_initial_clusters(points, clusters_count)
+	current_clusters = get_next_clusters(points, previous_clusters)
+	while not clusters_groups_are_same(points, previous_clusters, current_clusters):
+		previous_clusters = current_clusters
+		current_clusters = get_next_clusters(points, previous_clusters)
+
+	return group_points_by_clusters(points, current_clusters)
+
+
 points = get_random_points(points_count)
+clusters = clusterize(points, 3)
