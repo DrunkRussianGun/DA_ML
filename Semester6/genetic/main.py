0c455dbe0e7f5767268bf61b9796fb72363ef6d0
Drunk Russian Gun <drunkrussiangun@gmail.com>
2020-12-28 01:48:19 +0300

    Реализовал остальные функции

--- Semester6/genetic/main.py
+++ Semester6/genetic/main.py
@@ -12,7 +12,16 @@ class DiophantineEquation:
 		self.constant_term = constant_term
 
 	def __str__(self):
-		raise NotImplementedError()
+		def with_sign(coefficient: int) -> str:
+			sign = "-" if coefficient < 0 else "+"
+			return f" {sign} {abs(coefficient)}"
+
+		return str(self.coefficients[0]) + " x1" +\
+			"".join(
+				f"{with_sign(coefficient)} x{index}"
+				for index, coefficient in enumerate(self.coefficients[1:], start = 2)) +\
+			(with_sign(self.constant_term) if self.constant_term != 0 else "") +\
+			" = 0"
 
 
 class Solution:
@@ -22,25 +31,38 @@ class Solution:
 				f"Количество значений переменных {variable_values} должно совпадать " +
 				f"с количеством переменных в уравнении {equation}")
 		self.variable_values = variable_values
+		self._equation = equation
 		self._score = Solution._score(equation, variable_values)
 
 	@property
 	def score(self) -> float:
 		return self._score
 
-	def mutate(self):
-		raise NotImplementedError()
+	def mutate(self, mutant_solution: Solution):
+		value_mutation_probability = 1 / len(self.variable_values)
+		self.variable_values = [numpy.random.choice(
+				[old_value, new_value],
+				p = [value_mutation_probability, 1 - value_mutation_probability])
+			for old_value, new_value in zip(self.variable_values, mutant_solution.variable_values)]
+		self._score = Solution._score(self._equation, self.variable_values)
 
 	@staticmethod
 	def breed(first: Solution, second: Solution) -> Solution:
-		raise NotImplementedError()
+		return Solution(
+			first._equation,
+			[numpy.random.choice([first_value, second_value])
+				for first_value, second_value in zip(first.variable_values, second.variable_values)])
 
 	def __str__(self):
-		raise NotImplementedError()
+		return ", ".join(
+			f"x{index} = {value}"
+			for index, value in enumerate(self.variable_values, start = 1))
 
 	@staticmethod
 	def _score(equation: DiophantineEquation, variable_values: List[int]) -> float:
-		raise NotImplementedError()
+		return 1 / abs(
+			sum(numpy.multiply(equation.coefficients, variable_values)) +
+			equation.constant_term)
 
 
 def main():
@@ -84,10 +106,11 @@ def get_solution(equation: DiophantineEquation) -> Solution:
 			if child.score == 0:
 				return child
 
-		for solution in new_population:
+		mutant_population = generate_population(equation, new_population_size)
+		for index, solution in enumerate(new_population):
 			need_mutation = numpy.random.choice([True, False], p = [mutation_probability, 1 - mutation_probability])
 			if need_mutation:
-				solution.mutate()
+				solution.mutate(mutant_population[index])
 				if solution.score == 0:
 					return solution
 
